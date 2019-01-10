e26120c5255888cba65318345b84792aff8dd61d
Josue Peralta <jperal77@gmail.com>
2019-01-10 17:39:19 -0600

    Added functionality to the buttons in the Properties view

--- client/src/components/admin/properties/Properties.js
+++ client/src/components/admin/properties/Properties.js
@@ -1,31 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
 import {
   Edit,
   Delete,
   Home,
   Person,
   DateRange,
-  CheckCircleOutline
-} from '@material-ui/icons';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import AddPropertyCard from './AddPropertyCard';
+  CheckCircleOutline,
+  Close
+} from "@material-ui/icons";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Button from "@material-ui/core/Button";
+import AddPropertyCard from "./AddPropertyCard";
+import Modal from "@material-ui/core/Modal";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
 const styles = theme => ({
   container: {
     marginTop: 100
   },
   root: {
-    width: '100%',
+    width: "100%",
     maxWidth: 400,
     backgroundColor: theme.palette.background.paper
   },
@@ -34,46 +39,87 @@ const styles = theme => ({
     minHeight: 384
   },
   actions: {
-    display: 'flex',
-    justifyContent: 'flex-end'
+    display: "flex",
+    justifyContent: "flex-end"
+  },
+  displayNone: {
+    display: "none"
+  },
+  paper: {
+    width: "80%",
+    height: "80vh",
+    margin: "auto",
+    marginTop: 50
   }
 });
 
 class Properties extends React.Component {
   state = {
-    expanded: false,
+    detailedViewOn: false,
+    selectedProperty: {},
+    editModalOpen: false,
+    trashModalOpen: false,
     properties: [
       {
-        address: '171 N 600 E Provo, UT 84606',
-        tenants: ['Laura Carver', 'Tyler Carver'],
-        leaseDate: '6/13/17-6/23/18',
+        id: 1,
+        address: "171 N 600 E Provo, UT 84606",
+        tenants: ["Laura Carver", "Tyler Carver"],
+        leaseDate: "6/13/17-6/23/18",
         contract: true
       },
       {
-        address: '171 N 600 E Provo, UT 84606',
-        tenants: ['Laura Carver', 'Tyler Carver'],
-        leaseDate: '6/13/17-6/23/18',
+        id: 2,
+        address: "171 N 600 E Provo, UT 84606",
+        tenants: ["Laura Carver", "Tyler Carver"],
+        leaseDate: "6/13/17-6/23/18",
         contract: true
       },
       {
-        address: '171 N 600 E Provo, UT 84606',
-        tenants: ['Laura Carver', 'Tyler Carver'],
-        leaseDate: '6/13/17-6/23/18',
+        id: 3,
+        address: "171 N 600 E Provo, UT 84606",
+        tenants: ["Laura Carver", "Tyler Carver"],
+        leaseDate: "6/13/17-6/23/18",
         contract: true
       },
       {
-        address: '171 N 600 E Provo, UT 84606',
-        tenants: ['Laura Carver', 'Tyler Carver'],
-        leaseDate: '6/13/17-6/23/18',
+        id: 4,
+        address: "171 N 600 E Provo, UT 84606",
+        tenants: ["Laura Carver", "Tyler Carver"],
+        leaseDate: "6/13/17-6/23/18",
         contract: true
       },
       {
-        address: '171 N 600 E Provo, UT 84606',
-        tenants: ['Laura Carver', 'Tyler Carver'],
-        leaseDate: '6/13/17-6/23/18',
+        id: 5,
+        address: "171 N 600 E Provo, UT 84606",
+        tenants: ["Laura Carver", "Tyler Carver"],
+        leaseDate: "6/13/17-6/23/18",
         contract: true
       }
     ]
+  };
+
+  viewMore = event => {
+    console.log(event.currentTarget.getAttribute("data-id"));
+
+    if (
+      this.state.detailedViewOn !== true &&
+      this.state.selectedProperty.id !==
+        this.state.properties[event.currentTarget.getAttribute("data-id")]
+    ) {
+      this.setState({ detailedViewOn: true });
+    }
+  };
+
+  closeDetailedView = () => {
+    this.setState({ detailedViewOn: false });
+  };
+
+  toggleEditProperty = () => {
+    this.setState({ editModalOpen: !this.state.editModalOpen });
+  };
+
+  toggleRemoveProperty = () => {
+    this.setState({ trashModalOpen: !this.state.trashModalOpen });
   };
 
   render() {
@@ -82,16 +128,28 @@ class Properties extends React.Component {
 
     return (
       <Grid container className={classes.container} spacing={16}>
-        <Grid item xs={12}>
+        <Grid item xs={12} lg={this.state.detailedViewOn ? 8 : 12}>
           <Grid container justify="center" spacing={16}>
             {this.state.properties.map((entry, index) => (
-              <Grid key={index} item xs={12} sm={6} md={4}>
+              <Grid
+                key={index}
+                item
+                xs={12}
+                sm={6}
+                md={this.state.detailedViewOn ? 6 : 4}
+              >
                 <Card className={classes.card}>
                   <CardActions className={classes.actions} disableActionSpacing>
-                    <IconButton aria-label="Edit Property">
+                    <IconButton
+                      aria-label="Edit Property"
+                      onClick={this.toggleEditProperty}
+                    >
                       <Edit />
                     </IconButton>
-                    <IconButton aria-label="Delete Property">
+                    <IconButton
+                      aria-label="Delete Property"
+                      onClick={this.toggleRemoveProperty}
+                    >
                       <Delete />
                     </IconButton>
                   </CardActions>
@@ -112,7 +170,7 @@ class Properties extends React.Component {
                         </Avatar>
                         <ListItemText
                           primary="Tenant"
-                          secondary={entry.tenants.join(', ')}
+                          secondary={entry.tenants.join(", ")}
                         />
                       </ListItem>
                       <ListItem>
@@ -134,12 +192,52 @@ class Properties extends React.Component {
                         />
                       </ListItem>
                     </List>
+                    <Grid container justify="center">
+                      <Button
+                        data-id={index}
+                        onClick={this.viewMore}
+                        variant="outlined"
+                      >
+                        View More Info
+                      </Button>
+                    </Grid>
                   </CardContent>
                 </Card>
               </Grid>
             ))}
-            <AddPropertyCard />
+            <AddPropertyCard detailedViewOn={this.state.detailedViewOn} />
           </Grid>
+        </Grid>
+        <Grid
+          item
+          xs={4}
+          className={this.state.detailedViewOn ? "" : classes.displayNone}
+        >
+          <Grid container>
+            <IconButton onClick={this.closeDetailedView}>
+              <Close />
+            </IconButton>
+          </Grid>
+          <Modal
+            open={this.state.editModalOpen}
+            onClose={this.toggleEditProperty}
+          >
+            <Paper className={classes.paper}>
+              <Typography variant="h5" component="p">
+                Edit
+              </Typography>
+            </Paper>
+          </Modal>
+          <Modal
+            open={this.state.trashModalOpen}
+            onClose={this.toggleRemoveProperty}
+          >
+            <Paper className={classes.paper}>
+              <Typography variant="h5" component="p">
+                Remove
+              </Typography>
+            </Paper>
+          </Modal>
         </Grid>
       </Grid>
     );
