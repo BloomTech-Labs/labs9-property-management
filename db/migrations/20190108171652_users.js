17162bba24d3504099011244bf2824ff97ac8102
Keegan Burkett <keegan.t.burkett@gmail.com>
2019-01-10 12:32:59 -0600

    Removes card info fields from users table

--- db/migrations/20190108171652_users.js
+++ db/migrations/20190108171652_users.js
@@ -1,32 +1,24 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function(tbl) {
-      tbl.increments();
-      tbl.string('firstname', 64)
-        .notNullable();
-      tbl.string('middlename', 64);
-      tbl.string('lastname', 64)
-        .notNullable();
-      tbl.string('usertype', 24)
-        .notNullable();
-      tbl.string('email', 64)
-        .notNullable()
-        .unique('email');
-      tbl.integer('mobile')
-        .notNullable()
-        .unique('mobile');
-      tbl.string('password', 128)
-        .notNullable();
-      tbl.boolean('gettexts')
-        .defaultTo(false);
-      tbl.boolean('getemails')
-        .defaultTo(false);
-      tbl.integer('cc');
-      tbl.integer('expiration');
-      tbl.integer('cvv');
-  })
+  return knex.schema.createTable("users", function(tbl) {
+    tbl.increments();
+    tbl.string("firstname", 64).notNullable();
+    tbl.string("middlename", 64);
+    tbl.string("lastname", 64).notNullable();
+    tbl.string("usertype", 24).notNullable();
+    tbl
+      .string("email", 64)
+      .notNullable()
+      .unique("email");
+    tbl
+      .integer("mobile")
+      .notNullable()
+      .unique("mobile");
+    tbl.string("password", 128).notNullable();
+    tbl.boolean("gettexts").defaultTo(false);
+    tbl.boolean("getemails").defaultTo(false);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
+  return knex.schema.dropTable("users");
 };
