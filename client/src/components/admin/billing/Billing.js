8fde8579c48d5e4b63fcf10d4be2a655d408c119
Charlie Winslow <cwinslow22@yahoo.com>
2019-01-10 18:20:52 -0600

    added more detail to the admin billing view

--- client/src/components/admin/billing/Billing.js
+++ client/src/components/admin/billing/Billing.js
@@ -7,13 +7,33 @@ import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 const styles = theme => ({
   container: {
     display: "flex",
     flexWrap: "wrap",
+    justifyContent: "space-around",
     marginTop: 50,
     minHeight: "400px"
+    // border: "1px solid orange"
+  },
+  leftColumn: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    // border: "1px solid black",
+    flexDirection: "column"
+  },
+  rightColumn: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "flex-start"
   },
   textField: {
     marginLeft: theme.spacing.unit,
@@ -25,18 +45,25 @@ const styles = theme => ({
   },
   menu: {
     width: 200,
-    marginTop: 66
+    marginTop: 66,
+    marginBottom: 30
   },
   button: {
     width: 200,
     marginTop: 38
+  },
+  card: {
+    maxWidth: 300,
+    marginTop: 50,
+    paddingLeft: 30
   }
 });
 
 class Billing extends Component {
   state = {
     anchorEl: null,
-    property: [
+    selectedProperty: "171 N 600 E",
+    properties: [
       {
         id: 1,
         address: "171 N 600 E",
@@ -93,34 +120,99 @@ class Billing extends Component {
 
     return (
       <Grid container className={classes.container} spacing={16}>
-        <Grid item xs={12}>
-          <Grid container justify="center" spacing={16}>
-            <Button
-              className={classes.menu}
-              aria-owns={anchorEl ? "simple-menu" : undefined}
-              aria-haspopup="true"
-              onClick={this.handleClick}
-            >
-              Property
-            </Button>
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={this.handleClose}
-            >
-              <MenuItem onClick={this.handleClose}>
-                {this.state.property[0].address}
-              </MenuItem>
-              <MenuItem onClick={this.handleClose}>
-                {" "}
-                {this.state.property[1].address}
-              </MenuItem>
-              <MenuItem onClick={this.handleClose}>
-                {this.state.property[2].address}
-              </MenuItem>
-            </Menu>
-          </Grid>
+        <Grid className={classes.leftColumn} spacing={16}>
+          <Button
+            className={classes.menu}
+            aria-owns={anchorEl ? "simple-menu" : undefined}
+            aria-haspopup="true"
+            onClick={this.handleClick}
+          >
+            Property
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={this.handleClose}
+          >
+            <MenuItem onClick={this.handleClose}>
+              {this.state.properties[0].address}
+            </MenuItem>
+            <MenuItem onClick={this.handleClose}>
+              {this.state.properties[1].address}
+            </MenuItem>
+            <MenuItem onClick={this.handleClose}>
+              {this.state.properties[2].address}
+            </MenuItem>
+          </Menu>
+          <Card className={classes.card}>
+            <CardContent>
+              <List className={classes.root}>
+                <ListItem>
+                  <ListItemText
+                    primary="CC#"
+                    secondary={this.state.properties[0].cc}
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemText
+                    primary="EXP"
+                    secondary={this.state.properties[0].exp}
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemText
+                    primary="CVV"
+                    secondary={this.state.properties[0].cvv}
+                  />
+                </ListItem>
+              </List>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid className={classes.rightColumn} spacing={16}>
+          <Card className={classes.card}>
+            <Typography component="h1" variant="h6">
+              Date
+            </Typography>
+            <CardContent>
+              <List className={classes.root}>
+                <ListItem>
+                  <ListItemText primary="1/2/2018" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="2/2/2018" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="3/2/2018" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="4/2/2018" />
+                </ListItem>
+              </List>
+            </CardContent>
+          </Card>
+          <Card className={classes.card}>
+            <Typography component="h1" variant="h6">
+              Amount Paid
+            </Typography>
+            <CardContent>
+              <List className={classes.root}>
+                <ListItem>
+                  <ListItemText primary="$350.00" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="$350.00" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="$350.00" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="$350.00" />
+                </ListItem>
+              </List>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     );
