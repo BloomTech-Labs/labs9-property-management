22067d41008123e462b78acbf0a8e1323d6eb3c7
Josue Peralta <jperal77@gmail.com>
2019-01-14 00:49:45 -0600

    setup firebase auth api to allow registration via email/password and password reset/update

--- client/src/components/firebase/Firebase.js
+++ client/src/components/firebase/Firebase.js
@@ -1,4 +1,5 @@
 import app from "firebase/app";
+import "firebase/auth";
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,6 +14,20 @@ class Firebase {
   constructor() {
     app.initializeApp(config);
   }
+
+  // *** Auth API ***
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
