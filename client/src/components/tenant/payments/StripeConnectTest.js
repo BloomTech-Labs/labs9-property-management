3ebb695a4601cea8436c421e22f57cc826a9edde
Keegan Burkett <keegan.t.burkett@gmail.com>
2019-01-15 15:31:17 -0600

    Stripe connect is opening new tab and prompting user to create new stripe account

---
+++ client/src/components/tenant/payments/StripeConnectTest.js
@@ -0,0 +1,21 @@
+import React from "react";
+import connectwstripe from "../../../images/connect-with-stripe@2x.png";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const StripeButton = styled.img`
+  width: 144px;
+`;
+
+const StripeConnectTest = () => (
+  <Link
+    target="_blank"
+    to={
+      "//connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_ELLhp2vnlFHBpk0AVDL7PVxBzrsk2NXz&scope=read_write"
+    }
+  >
+    <StripeButton src={connectwstripe} />
+  </Link>
+);
+
+export default StripeConnectTest;
