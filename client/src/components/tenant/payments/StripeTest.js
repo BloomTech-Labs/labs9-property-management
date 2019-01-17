56dc0ae77e097c9243e8b6b05d1da8dd28e89895
Keegan Burkett <keegan.t.burkett@gmail.com>
2019-01-16 18:53:49 -0600

    Updates payment test form for check in

--- client/src/components/tenant/payments/StripeTest.js
+++ client/src/components/tenant/payments/StripeTest.js
@@ -18,7 +18,9 @@ class StripeTest extends React.Component {
       .post('http://localhost:4000/api/payments', body)
       .then(response => {
         console.log('response', response.data);
-        alert('Payment Success');
+        alert(
+          'Payment Success: token was received by backend and charge was made.'
+        );
         this.setState(prevState => {
           return {
             payments: prevState.payments.concat({
@@ -39,28 +41,13 @@ class StripeTest extends React.Component {
     this.setState({ [name]: value });
   };
 
-  handleUpdateAmount = event => {
-    event.preventDefault();
-    this.setState({ paymentAmount: this.state.paymentAmount });
-  };
-
   render() {
     const publishableKey = 'pk_test_IiM4lt5m1LYfjZBPfY8wa6Jo';
     console.log(this.state.payments);
     return (
       <>
-        <StripeCheckout
-          label="Pay rent" //Component button text
-          name="Property Mgmt" //Modal Header
-          description="Pay rent today."
-          panelLabel="Pay rent" //Submit button in modal
-          amount={Number(this.state.paymentAmount)} //Default state amount in cents $725.00
-          token={this.onToken}
-          stripeKey={publishableKey}
-          image={testlogo} //Pop-in header image
-          billingAddress={false}
-        />
-        <form onSubmit={this.handleUpdateAmount}>
+        <h3>Enter an amount then click make payment</h3>
+        <form>
           <input
             placeholder="Amount to pay"
             name="paymentAmount"
@@ -68,8 +55,20 @@ class StripeTest extends React.Component {
             value={this.state.paymentAmount}
             onChange={this.handleChange}
           />
-          <button>Update amount to pay</button>
         </form>
+        <br />
+        <StripeCheckout
+          label="Make payment" //Component button text
+          name="Property Mgmt" //Modal Header
+          description="Make a payment."
+          panelLabel="Make payment" //Submit button in modal
+          amount={Number(this.state.paymentAmount)} //Default state amount in cents $725.00
+          token={this.onToken}
+          stripeKey={publishableKey}
+          image={testlogo} //Pop-in header image
+          billingAddress={false}
+        />
+        <br />
         <h3>Payments</h3>
         {this.state.payments.map(payment => (
           <ul key={Date.now()}>
