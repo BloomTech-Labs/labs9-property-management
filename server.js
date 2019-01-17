afffdb34658b2020fa0f9928f17489f46cbea18b
Josue Peralta <jperal77@gmail.com>
2019-01-16 19:23:31 -0600

    changed initialization parameters for Firebase Admin to utilize .env

--- server.js
+++ server.js
@@ -21,10 +21,21 @@ server.use(morgan());
 
 //==== Firebase Admin ====
 const admin = require('firebase-admin');
-const servceAccount = require('./firebaseAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(servceAccount),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  }),
   databaseURL: process.env.FIREBASE_DB_URL,
 });
 
