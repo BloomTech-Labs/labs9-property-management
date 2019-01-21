65dcdc21ae7d82fd7505bbf3fbb955a013983446
Keegan Burkett <keegan.t.burkett@gmail.com>
2019-01-20 18:21:12 -0600

    Adds pricing card formatting and filler text

--- client/src/components/home/Nav.js
+++ client/src/components/home/Nav.js
@@ -3,7 +3,13 @@ import { Link } from 'react-router-dom';
 import testlogo from '../../images/test-logo.svg';
 import styled from 'styled-components';
 import MobileNav from './MobileNav';
+import { KeyboardBackspace } from '@material-ui/icons';
 
+const SignUpArrow = styled(KeyboardBackspace)`
+  color: #5f29ff;
+  transform: rotate(180deg);
+  margin-left: 4px;
+`;
 export const HeaderContainer = styled.div`
   position: fixed;
   width: 100%;
@@ -32,6 +38,8 @@ const SignUpLink = styled(Link)`
   justify-self: center;
   color: #5f29ff;
   font-weight: 500;
+  display: flex;
+  align-items: center;
   font-size: 1.6rem;
   text-decoration: none;
   @media (max-width: 500px) {
@@ -118,6 +126,7 @@ class Header extends React.Component {
                 </HeaderLink>
                 <SignUpLink desktop="true" to="/signup">
                   Sign Up
+                  <SignUpArrow />
                 </SignUpLink>
                 {/* <MobileMenuLink onClick={""} mobile>
                   Menu
--- client/src/components/home/subpages/Pricing.js
+++ client/src/components/home/subpages/Pricing.js
@@ -10,9 +10,11 @@ const PricingCardContainer = styled.div`
   flex-direction: row;
   justify-content: center;
   padding-bottom: 48px;
+  padding-top: 32px;
   @media (max-width: 680px) {
     flex-direction: ${props => (props.mobile ? 'column' : 'row')};
     align-items: center;
+    padding-top: 0px;
   }
 `;
 
@@ -21,15 +23,39 @@ const PricingCard = styled.div`
   border: 1px solid #eee;
   padding: 24px;
   flex-direction: column;
-  height: 400px;
-  width: 300px;
-  margin: 24px;
+  height: 375px;
+  width: 275px;
+  padding: 24px;
+  margin: 16px;
   border-radius: 4px;
-  background: ${props => (props.bestvalue ? 'white' : '#f9f9f9')};
+  background: ${props => (props.bestvalue ? 'white' : '#fbfbfb')};
 `;
-// const PricingTextLine = styled.div`
-//   display: block;
-// `;
+const PricingTextLine = styled.div`
+  display: block;
+  color: #333;
+  &.planName {
+    font-size: 32px;
+    font-weight: 700;
+  }
+  &.planNameSubText {
+    font-size: 24px;
+    font-weight: 400;
+    margin-bottom: 24px;
+    color: #777d8c;
+  }
+  &.planLineItem {
+    font-size: 16px;
+    font-weight: 400;
+    color: #2c303b;
+    line-height: 2.2;
+  }
+`;
+
+// Unlimited queries
+// Unlimited folders
+// 1 dashboard
+// No scheduled reports for email and Slack
+// No team members
 
 class Pricing extends Component {
   render() {
@@ -46,8 +72,54 @@ class Pricing extends Component {
             </HeroP>
           </HeroGroup>
           <PricingCardContainer mobile>
-            <PricingCard bestvalue />
-            <PricingCard />
+            <PricingCard bestvalue>
+              <PricingTextLine className="planName">Basic</PricingTextLine>
+              <PricingTextLine className="planNameSubText">
+                Free
+              </PricingTextLine>
+              <PricingTextLine className="planLineItem">
+                Unlimited queries
+              </PricingTextLine>
+              <PricingTextLine className="planLineItem">
+                No scheduled reports for email
+              </PricingTextLine>
+              <PricingTextLine className="planLineItem">
+                Up to 10 properties
+              </PricingTextLine>
+              <PricingTextLine className="planLineItem">
+                SMS maintenance alerts
+              </PricingTextLine>
+            </PricingCard>
+            <PricingCard>
+              <PricingTextLine className="planName">Growth</PricingTextLine>
+              <PricingTextLine className="planNameSubText">
+                $10 / admin / month
+              </PricingTextLine>
+              <PricingTextLine className="planLineItem">
+                Unlimited queries
+              </PricingTextLine>
+              <PricingTextLine className="planLineItem">
+                No scheduled reports for email
+              </PricingTextLine>
+              <PricingTextLine className="planLineItem">
+                Up to 10 properties
+              </PricingTextLine>
+              <PricingTextLine className="planLineItem">
+                SMS maintenance alerts
+              </PricingTextLine>
+              <PricingTextLine className="planLineItem">
+                Some more info here
+              </PricingTextLine>
+              <PricingTextLine className="planLineItem">
+                Another great free feature
+              </PricingTextLine>
+              <PricingTextLine className="planLineItem">
+                SMS maintenance alerts
+              </PricingTextLine>
+              <PricingTextLine className="planLineItem">
+                Some more info here
+              </PricingTextLine>
+            </PricingCard>
           </PricingCardContainer>
         </Hero>
         <Footer />
