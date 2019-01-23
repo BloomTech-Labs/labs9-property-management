a6b81993f86e06dc4f3d071d6239d4d436902585
Keegan Burkett <keegan.t.burkett@gmail.com>
2019-01-23 12:58:38 -0600

    Accounts are being created, working on inserting stripe uid into owners table

--- routes/stripeConnectRoutes.js
+++ routes/stripeConnectRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('../db/dbConfig');
 const router = express.Router();
 const axios = require('axios');
 
@@ -6,7 +7,7 @@ router.use(express.json());
 
 // receive auth code from stripe from the redirect url (route on our server that will be receiving the code from stripe)
 router.post('/', (req, res) => {
-  console.log(req.body, 'UID');
+  const { uid } = req.body;
   axios
     .post('https://connect.stripe.com/oauth/token', {
       grant_type: 'authorization_code',
@@ -14,7 +15,20 @@ router.post('/', (req, res) => {
       client_secret: process.env.STRIPE_SECRET_KEY,
       code: req.body.computedCode,
     })
-    .then(response => console.log('RESPONSE:', response))
+    .then(response => {
+      const { stripe_user_id } = response.data;
+      const stripe_info = { stripe_user_id: stripe_user_id };
+      /*console.log('RESPONSE:', response.data, req.body.uid)*/
+      db.insert(stripe_info)
+        .into('owners')
+        .where('owners.owner_uid', uid)
+        .then(owner => {
+          if (!owner) {
+            res.status(401).json({ message: 'Owner does not exist' });
+            return;
+          } else res.status(200).json(owner);
+        });
+    })
     .catch(err => console.log('ERROR:', err));
 });
 
