ff5c4bd28b4da51174a7040706098767b6c79c84
Keegan Burkett <keegan.t.burkett@gmail.com>
2019-01-24 19:14:43 -0600

    Connect with stripe and connected are conditionally rendering depending on if stripe uid is found in db

--- client/src/components/admin/billing/Billing.js
+++ client/src/components/admin/billing/Billing.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { withAuthUser } from '../../session';
+import { compose } from 'recompose';
 import axios from 'axios';
 import connectwstripe from '../../../images/connect-with-stripe@2x.png';
 import styled from 'styled-components';
@@ -33,7 +35,7 @@ const styles = theme => ({
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
-    marginTop: 50,
+    marginTop: 200,
     minHeight: '400px',
     // border: "1px solid orange"
   },
@@ -68,7 +70,7 @@ const styles = theme => ({
   },
   card: {
     maxWidth: 300,
-    marginTop: 50,
+    marginTop: 200,
     paddingLeft: 30,
   },
   root: {
@@ -99,126 +101,76 @@ const styles = theme => ({
 
 class Billing extends Component {
   state = {
-    hasStripeID: null,
+    hasStripeID: false,
     fetchingStripeID: true,
-    property: '171 N 600 E',
-    cc: '1234567812345678',
-    exp: '09/20',
-    cvv: '***',
-    history: [
-      { date: '1/2/2018', amount: '$350.00' },
-      { date: '2/1/2018', amount: '$350.00' },
-      { date: '3/3/2018', amount: '$350.00' },
-      { date: '4/3/2018', amount: '$350.00' },
-    ],
-    properties: [
-      {
-        id: 1,
-        address: '171 N 600 E',
-        cc: '1234567812345678',
-        exp: '09/19',
-        cvv: '***',
-        history: [
-          { date: '1/2/2018', amount: '$350.00' },
-          { date: '2/1/2018', amount: '$350.00' },
-          { date: '3/3/2018', amount: '$350.00' },
-          { date: '4/3/2018', amount: '$350.00' },
-        ],
-      },
-      {
-        id: 2,
-        address: '990 Smith Ave',
-        cc: '1123445677650989',
-        exp: '07/19',
-        cvv: '***',
-        history: [
-          { date: '1/2/2018', amount: '$390.00' },
-          { date: '2/2/2018', amount: '$390.00' },
-          { date: '3/2/2018', amount: '$390.00' },
-          { date: '4/4/2018', amount: '$390.00' },
-          { date: '5/1/2018', amount: '$390.00' },
-        ],
-      },
-      {
-        id: 3,
-        address: '124 Elm Ave',
-        cc: '1455445677650989',
-        exp: '08/19',
-        cvv: '***',
-        history: [
-          { date: '1/3/2018', amount: '$440.00' },
-          { date: '2/2/2018', amount: '$440.00' },
-          { date: '3/3/2018', amount: '$440.00' },
-          { date: '4/2/2018', amount: '$440.00' },
-          { date: '5/3/2018', amount: '$440.00' },
-        ],
-      },
-    ],
   };
   componentDidMount() {
     console.log('props', this.props);
     // check to see if owner has stripe account connected
-    setTimeout(
-      () =>
-        axios
-          .get('/api/stripe-connect')
-          .then(response =>
-            response.data.hasStripeID === true
-              ? this.setState({ hasStripeID: true, fetchingStripeID: false })
-              : this.setState({ hasStripeID: false, fetchingStripeID: false })
-          )
-          .catch(err => console.log('ERROR CHECKING USER STRIPE ID', err)),
-      2000
-    );
-    // check to see if auth code is provided
-    if (this.props.location.search) {
-      console.log('BINGO', this.props.location.search.substring(23));
-      let computedCode = this.props.location.search.substring(23);
-      const stripeAuthCode = {
-        computedCode,
-      };
-      console.log(stripeAuthCode);
-      setTimeout(
-        () =>
-          axios
-            .post('/api/stripe-connect', stripeAuthCode)
-            .then(response => console.log('response'))
-            .catch(err => console.log(err)),
-        2000
-      );
+    if (this.props.authTokenRecieved) {
+      axios
+        .get('/api/stripe-connect')
+        .then(response => {
+          console.log('SERVER STRIPE ID:', response.data);
+          if (response.data.hasStripeID === true) {
+            this.setState({ hasStripeID: true, fetchingStripeID: false });
+          }
+          // check to see if auth code is provided
+          if (this.props.location.search) {
+            console.log('BINGO', this.props.location.search.substring(23));
+            let computedCode = this.props.location.search.substring(23);
+            const stripeAuthCode = {
+              computedCode,
+            };
+            console.log(stripeAuthCode);
+
+            axios.post('/api/stripe-connect', stripeAuthCode).then(response => {
+              this.setState({ hasStripeID: true, fetchingStripeID: false });
+            });
+          }
+        })
+        .catch(err => console.log('ERROR CHECKING USER STRIPE ID', err));
     }
   }
 
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value,
-      cc: this.state.properties.find(
-        entry => entry.address === event.target.value
-      ).cc,
-      exp: this.state.properties.find(
-        entry => entry.address === event.target.value
-      ).exp,
-      cvv: this.state.properties.find(
-        entry => entry.address === event.target.value
-      ).cvv,
-      history: this.state.properties.find(
-        entry => entry.address === event.target.value
-      ).history,
-    });
-    console.log(
-      this.state.properties.find(entry => entry.address === event.target.value)
-    );
-    console.log(
-      this.state.properties.find(entry => entry.address === event.target.value)
-        .cc
-    );
-    console.log(event.target.value);
-  };
+  componentDidUpdate(prevProps) {
+    if (
+      this.props.authTokenRecieved &&
+      this.props.authTokenRecieved !== prevProps.authTokenRecieved
+    ) {
+      axios
+        .get('/api/stripe-connect')
+        .then(response => {
+          if (response.data.hasStripeID === true) {
+            this.setState({ hasStripeID: true, fetchingStripeID: false });
+          } else {
+            // check to see if auth code is provided
+            if (this.props.location.search) {
+              console.log('BINGO', this.props.location.search.substring(23));
+              let computedCode = this.props.location.search.substring(23);
+              const stripeAuthCode = {
+                computedCode,
+              };
+              console.log(stripeAuthCode);
+
+              axios
+                .post('/api/stripe-connect', stripeAuthCode)
+                .then(response => {
+                  this.setState({ hasStripeID: true, fetchingStripeID: false });
+                });
+            } else {
+              this.setState({ hasStripeID: false, fetchingStripeID: false });
+            }
+          }
+        })
+        .catch(err => console.log('ERROR CHECKING USER STRIPE ID', err));
+    }
+  }
 
   render() {
     const { classes } = this.props;
-    console.log('STATE', this.state.hasStripeID);
     let stripeConnectionDetails;
+    console.log(this.state.hasStripeID);
 
     if (this.state.hasStripeID) {
       stripeConnectionDetails = (
@@ -234,97 +186,27 @@ class Billing extends Component {
             '//connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_ELLhp2vnlFHBpk0AVDL7PVxBzrsk2NXz&scope=read_write'
           }
         >
-          {' '}
           <StripeButton src={connectwstripe} />
         </Link>
       );
     }
 
     return (
-      <Grid container className={classes.container} spacing={16}>
-        <Grid className={classes.leftColumn}>
-          <form className={classes.root} autoComplete="off">
-            <FormControl variant="outlined" className={classes.formControl}>
-              <InputLabel>Property</InputLabel>
-              <Select
-                value={this.state.property}
-                onChange={this.handleChange}
-                input={
-                  <OutlinedInput
-                    name="property"
-                    id="outlined-simple"
-                    labelWidth={2}
-                  />
-                }
-              >
-                {/* <MenuItem value="">
-                  <em>None</em>
-                </MenuItem> */}
-                {this.state.properties.map((addr, index) => (
-                  <MenuItem key={index} value={addr.address}>
-                    {addr.address}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </form>
-          <Card className={classes.card}>
-            <CardContent>
-              {this.state.fetchingStripeID ? (
-                <Typography
-                  className={classes.tableTitle}
-                  component="h6"
-                  variant="h6"
-                >
-                  Loading stripe details...
-                </Typography>
-              ) : (
-                stripeConnectionDetails
-              )}
-              <List className={classes.root}>
-                <ListItem>
-                  <ListItemText primary="CC#" secondary={this.state.cc} />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="EXP" secondary={this.state.exp} />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="CVV" secondary={this.state.cvv} />
-                </ListItem>
-              </List>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid className={classes.rightColumn}>
-          <Paper className={classes.rootTable}>
+      <Card className={classes.card}>
+        <CardContent>
+          {this.state.fetchingStripeID ? (
             <Typography
               className={classes.tableTitle}
-              component="h5"
+              component="h6"
               variant="h6"
             >
-              Rent History
+              Loading stripe details...
             </Typography>
-            <Table className={classes.table}>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Date</TableCell>
-                  <TableCell align="right">Amount Paid</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {this.state.history.map((entry, index) => (
-                  <TableRow key={index}>
-                    <TableCell component="th" scope="row">
-                      {entry.date}
-                    </TableCell>
-                    <TableCell align="right">{entry.amount}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Paper>
-        </Grid>
-      </Grid>
+          ) : (
+            stripeConnectionDetails
+          )}
+        </CardContent>
+      </Card>
     );
   }
 }
@@ -333,4 +215,9 @@ Billing.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(Billing);
+const BillingPage = compose(
+  withAuthUser,
+  withStyles(styles)
+)(Billing);
+
+export default BillingPage;
