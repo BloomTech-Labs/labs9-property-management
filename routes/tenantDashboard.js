45555713ebd76cd507e2628a8797425b1aa1747e
Charlie Winslow <cwinslow22@yahoo.com>
2019-01-28 18:48:56 -0600

    tenant maintenance view now gets property info from backend and can make a post to the work orders table

--- routes/tenantDashboard.js
+++ routes/tenantDashboard.js
@@ -11,12 +11,37 @@ router.get('/', (req, res) => {
 
   db('tenants as t')
     .where('tenant_uid', uid)
-    .join('house_properties as h', 'house_id', 't.house_id')
-    .select('h.address', 'h.office_ph', 'h.maintenance_ph')
+    .join('house_properties as h', 'h.house_id', 't.house_id')
+    .join('users as u', 'u.uid', 't.tenant_uid')
+    .select(
+      'h.address',
+      'h.office_ph',
+      'h.maintenance_ph',
+      'u.mobile',
+      't.house_id',
+      't.tenant_id'
+    )
     .then(property => {
       res.status(200).json(property);
     })
     .catch(error => res.status(500).json(error));
+});
+
+// Manually update tenant house_id in order to test other views that require the house_id info
+router.put('/update', (req, res) => {
+  const { uid, house_id } = req.body;
+  const house = { house_id: house_id };
+
+  db('tenants as t')
+    .where('t.tenant_uid', uid)
+    .update(house)
+    .then(tenant => {
+      if (!tenant) {
+        res.status(401).json({ message: 'Tenant does not exist' });
+        return;
+      } else res.status(200).json(tenant);
+    })
+    .catch(err => console.log('Error:', err));
 });
 
 module.exports = router;
