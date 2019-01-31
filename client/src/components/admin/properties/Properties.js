620b2310cfaa5a48df0492fa75bc2eb6e86d7142
Josue Peralta <jperal77@gmail.com>
2019-01-30 23:26:32 -0600

    added a searchbar

--- client/src/components/admin/properties/Properties.js
+++ client/src/components/admin/properties/Properties.js
@@ -18,6 +18,8 @@ import {
   CheckCircleOutline,
   Close,
 } from '@material-ui/icons';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -31,6 +33,7 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
+import Hidden from '@material-ui/core/Hidden';
 import CustomSnackbar from '../../snackbar/CustomSnackbar';
 import axios from 'axios';
 
@@ -39,7 +42,7 @@ const styles = theme => ({
     marginTop: 100,
     marginLeft: 0,
   },
-  root: {
+  list: {
     width: '100%',
     maxWidth: 400,
     backgroundColor: theme.palette.background.paper,
@@ -67,17 +70,25 @@ const styles = theme => ({
     margin: 'auto',
     marginTop: 50,
   },
-  absolute: {
-    position: 'absolute',
-    bottom: theme.spacing.unit * 2,
-    right: theme.spacing.unit * 3,
-  },
   dialog: {
     display: 'flex',
     justifyContent: 'center',
   },
   button: {
     margin: theme.spacing.unit,
+  },
+  searchbar: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+  },
+  input: {
+    marginLeft: 8,
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
   },
 });
 
@@ -219,17 +230,43 @@ class Properties extends React.Component {
     const { classes } = this.props;
     console.log('properties', this.state.properties);
 
+    const addBtnClass = {
+      display: 'flex',
+      width: '100%',
+      justifyContent: 'center',
+    };
+
     return (
       <Grid container className={classes.container} spacing={16}>
         <Grid item xs={12}>
-          <Button
-            variant="contained"
-            color="primary"
-            className={classes.button}
-            onClick={this.toggleAddProperty}
-          >
-            Add Property
-          </Button>
+          <Grid container justify="space-between" spacing={0}>
+            <Grid item xs={12} md={4}>
+              <Paper className={classes.searchbar} elevation={1}>
+                <InputBase
+                  className={classes.input}
+                  placeholder="Search by Name"
+                />
+                <IconButton className={classes.iconButton} aria-label="Search">
+                  <SearchIcon />
+                </IconButton>
+              </Paper>
+            </Grid>
+            <Hidden mdDown>
+              <Grid item xs={4} />
+            </Hidden>
+            <Grid item xs={12} md={2}>
+              <div style={addBtnClass}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.button}
+                  onClick={this.toggleAddProperty}
+                >
+                  Add Property
+                </Button>
+              </div>
+            </Grid>
+          </Grid>
         </Grid>
         <Grid item xs={12} lg={this.state.detailedViewOn ? 8 : 12}>
           <Grid container justify="center" spacing={16}>
@@ -276,7 +313,7 @@ class Properties extends React.Component {
                       <Typography variant="h5" align="center" component="h6">
                         {entry.property_name}
                       </Typography>
-                      <List className={classes.root}>
+                      <List className={classes.list}>
                         <ListItem>
                           <Avatar>
                             <Home />
