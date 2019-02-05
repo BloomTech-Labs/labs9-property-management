b84d44f6270fc5950348f833db0a087727dec225
Keegan Burkett <keegan.t.burkett@gmail.com>
2019-02-04 19:41:09 -0600

    Adds working mobile nav, not perfect yet, but works

--- client/src/components/home/MobileNav.js
+++ client/src/components/home/MobileNav.js
@@ -1,56 +1,91 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import { Logo, HeaderLink } from "./Nav";
-import testlogo from "../../images/test-logo.svg";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Logo, HeaderLink } from './Nav';
+import testlogo from '../../images/test-logo.svg';
 
 const MobileNavContainer = styled.div`
   height: 100vh;
   width: 100vw;
   background: white;
+  z-index: 1000;
+  margin: 0 auto;
+  padding: 0 4rem;
+  overflow: auto;
 `;
 const MobileNavGroup = styled.div`
-  display: grid;
+  display: flex;
+  justify-content: center;
+  justify-self: ${props => (props.close ? 'flex-start' : 'center')};
+  flex-direction: column;
   align-items: center;
-  margin: 0 auto;
-  padding: 0;
   font-weight: 500;
-  grid-template-columns: 1fr 1fr;
+`;
+
+const PageLinkContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 80vh;
+  flex-direction: column;
 `;
 
 const MobileLink = styled(Link)`
   color: rgba(73, 76, 87, 1);
   text-decoration: none;
-  font-weight: 500;
-  display: grid;
-  justify-items: center;
-  font-size: 2.4rem;
+  padding: ${props => (props.close ? '1rem' : '2rem')};
+  font-weight: ${props => (props.close ? '500' : '700')};
+  text-align: left;
+  cursor: pointer;
+  font-size: ${props => (props.close ? '2.4rem' : '3.6rem')};
+  color: rgba(22, 23, 26, 1);
 `;
 const CloseNav = styled.span`
   background: none;
   border: none;
+  cursor: pointer;
   color: #874ef4;
   font-weight: 500;
   font-size: 1.6rem;
   text-decoration: none;
-  justify-self: end;
 `;
 
-const MobileNav = props => (
-  <MobileNavContainer>
-    <MobileNavGroup>
-      <HeaderLink to="/">
-        <Logo src={testlogo} alt="logo" width="20" />
-      </HeaderLink>
-      {/* <CloseNav onClick={() => props.handleMenuToggle()} to="/">
-        Close
-      </CloseNav> */}
-      <CloseNav>
-        Close
-      </CloseNav>
-    </MobileNavGroup>
-    <MobileLink to="/">Home</MobileLink>
-  </MobileNavContainer>
-);
+class MobileNav extends React.Component {
+  render() {
+    return (
+      <MobileNavContainer>
+        <MobileNavGroup close>
+          <CloseNav close onClick={() => this.props.handleMenuToggle()}>
+            Close
+          </CloseNav>
+        </MobileNavGroup>
+        <PageLinkContainer>
+          <MobileNavGroup>
+            <MobileLink onClick={() => this.props.handleMenuToggle()} to="/">
+              Home
+            </MobileLink>
+            <MobileLink
+              onClick={() => this.props.handleMenuToggle()}
+              to="/pricing"
+            >
+              Pricing
+            </MobileLink>
+            <MobileLink
+              onClick={() => this.props.handleMenuToggle()}
+              to="/login"
+            >
+              Login
+            </MobileLink>
+            <MobileLink
+              onClick={() => this.props.handleMenuToggle()}
+              to="/signup"
+            >
+              Signup
+            </MobileLink>
+          </MobileNavGroup>
+        </PageLinkContainer>
+      </MobileNavContainer>
+    );
+  }
+}
 
 export default MobileNav;
